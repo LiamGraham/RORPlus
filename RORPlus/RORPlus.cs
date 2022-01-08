6214a1fa77138b0ff1bb22416339da49d857a425
Liam Graham <liam.david.graham@gmail.com>
2022-01-08 21:14:46 +1000

    Change main class name to RORPlus

--- RORPlus/RORPlus.cs
+++ RORPlus/RORPlus.cs
@@ -4,13 +4,12 @@ using R2API;
 namespace RORPlus
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
+    public class RORPlus : BaseUnityPlugin
     {
         private void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-            Logger.LogInfo("Risk of Rain 2 Plus (RORPlus) loaded");
         }
     }
 }
