cf51a4d40b42cfc2addc0effe491ffc76041fa7c
Liam Graham <liam.david.graham@gmail.com>
2022-01-09 18:44:28 +1000

    Prototype baseJumpCount adjustment

--- RORPlus/RORPlus.cs
+++ RORPlus/RORPlus.cs
@@ -7,10 +7,36 @@ namespace RORPlus
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class RORPlus : BaseUnityPlugin
     {
-        private void Awake()
+        void Awake()
         {
-            // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        void Start() {
+            RoR2.Run.onRunStartGlobal += (run) =>
+            {
+                SetMinimumBaseJumpCount(2);
+            };
+        }
+
+        private void SetMinimumBaseJumpCount(int count) {
+            foreach (SurvivorDef survivor in RoR2.SurvivorCatalog.allSurvivorDefs)
+            {
+                CharacterBody body = survivor.bodyPrefab.GetComponent<CharacterBody>();
+                var actualCount = count;
+                if (survivor.cachedName.Equals("Merc"))
+                {
+                    // Ensure that Merc has one more jump than all other survivors
+                    actualCount += 1;
+                }
+                if (body.baseJumpCount <= actualCount)
+                {
+                    // Prevent new base jump count being smaller than existing base for survivor
+                    body.baseJumpCount = actualCount;
+
+                }
+                Logger.LogInfo($"baseJumpCount of {survivor.cachedName} is {body.baseJumpCount}");
+            }
         }
     }
 }
